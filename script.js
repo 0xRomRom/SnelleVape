f3cce0bc0ff0db58b45ebd33b6349598828c89f8
0xromrom <roman0811@live.nl>
2022-08-08 07:30:43 -0700

    Added second section and fixed a few bugs

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const menuOptions = document.querySelector(".menu-options");
-const burgerMenu = document.querySelector(".burger-button");
+const burgerMenu = document.querySelector(".menu");
 const shoppingCart = document.querySelector(".shopping-cart");
 
 burgerMenu.addEventListener("click", () => {
@@ -10,3 +10,19 @@ burgerMenu.addEventListener("click", () => {
   shoppingCart.style.display =
     shoppingCart.style.display != "none" ? "none" : "flex";
 });
+
+// const findOverflows = () => {
+//   const documentWidth = document.documentElement.offsetWidth;
+
+//   document.querySelectorAll("*").forEach((element) => {
+//     const box = element.getBoundingClientRect();
+
+//     if (box.left < 0 || box.right > documentWidth) {
+//       console.log(element);
+//       element.style.border = "1px solid red";
+//     }
+//   });
+// };
+
+// // Execute findOverflows to find overflows on the page.
+// findOverflows();
