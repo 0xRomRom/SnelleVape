487cf596d22e04d432848880f53c34a3558fb5bc
0xromrom <roman0811@live.nl>
2022-08-08 05:45:19 -0700

    Mobile hamburger menu working

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 "use strict";
-"use strict";
+
+const menuOptions = document.querySelector(".menu-options");
+const burgerMenu = document.querySelector(".burger-button");
+const shoppingCart = document.querySelector(".shopping-cart");
+
+burgerMenu.addEventListener("click", () => {
+  menuOptions.style.display =
+    menuOptions.style.display != "flex" ? "flex" : "none";
+  shoppingCart.style.display =
+    shoppingCart.style.display != "none" ? "none" : "flex";
+});
